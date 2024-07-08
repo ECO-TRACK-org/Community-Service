af091260da134f87e1dc9fb2be9a300f9320e8f2
Azizbek <146860466+pronium@users.noreply.github.com>
2024-07-08 11:55:12 +0500

    Add files via upload

---
+++ cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"EcoTrack/community/config"
+	p "EcoTrack/community/genproto/protos"
+	"EcoTrack/community/pkg/db"
+	s "EcoTrack/community/service"
+	storage "EcoTrack/community/storage/postgres"
+
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	config := config.Load()
+
+	db, err := db.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	listener, err := net.Listen("tcp", config.URL_PORT)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+	log.Println("server is running on " + config.URL_PORT)
+
+	server := grpc.NewServer()
+
+	storage := storage.NewCommunityRepo(db)
+	service := s.CommunityService{Storage: *storage}
+
+	p.RegisterCommunityServiceServer(server, &service)
+
+	server.Serve(listener)
+
+}
